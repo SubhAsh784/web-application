98a2811823ed556025b143d4fd81996c6e70107d
SubhAsh784 <subhashraj106@gmail.com>
2024-07-13 00:43:55 +0530

    Add files via upload

---
+++ CodequizApplication.java
@@ -0,0 +1,16 @@
+package com.quiz.codequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodequizApplication{
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+
+		SpringApplication.run(CodequizApplication.class, args);
+	}
+
+}
